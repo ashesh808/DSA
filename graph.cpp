d417b93fb7d084cc75affd050f6ca056934e5234
Student <asheshusa9@gmail.com>
2022-07-03 22:57:06 -0500

    Added default template for graph data structure

---
+++ graph.cpp
@@ -0,0 +1,29 @@
+// Including the required headers
+#include <iostream>
+using namespace std;
+
+// This class represents the node of a graph
+class Node
+{
+public:
+    // Data memebers of a node are:
+    //  1. Key : which is used for finding and updating each node will have a unique key
+    //  2. Data : which is the actual value contained in the node
+    int key;
+    int data;
+
+    // Constructor methods
+    Node();
+    Node(int k, int d);
+};
+
+// This class to represent the data structure Graph
+class Graph
+{
+public:
+};
+
+int main()
+{
+    return 0;
+}
